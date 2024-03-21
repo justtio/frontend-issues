d63c4f91b7b0009c700f5ff8e80c3b40e6c771ea
rivaille <xiaxiu404@163.com>
2024-03-21 11:11:24 +0800

    Update day1

--- Day1.js
+++ Day1.js
@@ -23,3 +23,67 @@ if(data) {
 }
 
 
+// new操作符做了什么？
+
+// 1. 创建一个空对象
+// 2. 将这个空对象的原型指向构造函数的prototype
+// 3. 将这个空对象作为this的上下文
+// 4. 如果构造函数没有返回对象，就返回this
+
+// 代码如下：
+function myNew() {
+    const obj = {};
+    const Constructor = [].shift.call(arguments);
+    obj.__proto__ = Constructor.prototype;
+    const result = Constructor.apply(obj, arguments);
+    return result instanceof Object ? result : obj;
+}
+
+// 使用方法如下：
+function Person(name) {
+    this.name = name;
+}
+
+const person = myNew(Person, 'Tom');
+
+//DNS解析的过程
+
+// DNS解析是将域名解析成IP地址的过程，主要分为以下几个步骤：
+// 1. 用户在浏览器中输入一个域名，比如www.baidu.com
+// 2. 操作系统首先会检查本地缓存中是否有该域名对应的IP地址，如果有，就直接返回
+// 3. 如果本地缓存中没有，就会向本地配置的DNS服务器发送一个DNS查询请求
+// 4. DNS服务器收到请求后，如果自己有缓存，就直接返回
+// 5. 如果没有，就会向根域名服务器发送请求
+// 6. 根域名服务器收到请求后，会返回一个顶级域名服务器的地址
+// 7. DNS服务器再向顶级域名服务器发送请求
+// 8. 顶级域名服务器收到请求后，会返回一个二级域名服务器的地址
+// 9. DNS服务器再向二级域名服务器发送请求
+// 10. 二级域名服务器收到请求后，会返回一个IP地址
+// 11. DNS服务器最后将IP地址返回给操作系统
+// 12. 操作系统将IP地址缓存起来，并返回给浏览器
+// 13. 浏览器拿到IP地址后，就可以向该IP地址发送请求了
+
+// 代码如下：
+// www.baidu.com ->
+// 本地缓存 -> 本地DNS服务器 -> 根域名服务器 -> 顶级域名服务器 -> 二级域名服务器 -> IP地址
+
+
+// 写一个完备的深拷贝函数
+function deepClone(obj, hash = new WeakMap()) {
+    if(obj === null) return obj;
+    if(obj instanceof Date) return new Date(obj);
+    if(obj instanceof RegExp) return new RegExp(obj);
+    if(typeof obj !== 'object') return obj;
+    if(hash.has(obj)) return hash.get(obj);
+
+    let cloneObj = new obj.constructor;
+    hash.set(obj, cloneObj);
+
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            cloneObj[key] = deepClone(obj[key], hash);
+        }
+    }
+
+    return cloneObj;
+}
